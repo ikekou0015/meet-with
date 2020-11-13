4d1e1cfd5e4968923e1f40261a0f7ae0d7c8ba85
ikekou0015 <ikekou0015@gmail.com>
2020-11-13 23:07:21 +0900

    ユーザー登録保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :image
 
-  
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
 end
